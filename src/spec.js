c00e9c7b04e5bb27b224d3e21e2428112a062e44
cztamas <tamas.czank@gmail.com>
2017-07-24 17:43:02 +0200

    tests partly corrected

--- src/spec.js
+++ src/spec.js
@@ -1,8 +1,10 @@
 "use strict";
 
 var ko = require("knockout");
-var checkCreator = require("./index.js");
-var check;
+var superschema = require("./index.js");
+var check = superschema.check;
+
+var defaultName = "configObject";
 
 var testObject = {
 	a: "some string",
@@ -22,40 +24,84 @@ var testObject = {
 	}),
 	i: true,
 	j: [1, 2, 3],
-	k: ko.observable(null)
+	k: ko.observable(null),
+	l: null
 };
 
-describe("invalid dependencies", function() {
-	it("throws error if invalid ko object is given", function() {
-		expect(function() {
-			checkCreator({
-				ko: "not really an object"
-			});
-		}).toThrowError("Invalid 'ko' parameter given!");
-
-		expect(function() {
-			checkCreator({
-				ko: {
-					isObservable: "not really a function"
-				}
-			});
-		}).toThrowError("Invalid 'ko' parameter given!");
-	});
-});
-
-describe("valid dependencies - with ko", function() {	
-	beforeEach(function() {
-		check = checkCreator({
-			ko: ko
-		});		
+describe("superschema tests", function() {
+	it("has correct interface", function() {
+		expect(typeof superschema).toBe("object");
+		expect(typeof superschema.check).toBe("function");
+		expect(typeof superschema.extend).toBe("function");
 	});
 
-	describe("test the parameters given", function() {
-		it ("throws error if the pattern isn't an object", function() {
-			var pattern = "not remotely an object";
+	describe("invalid extend usage", function() {
+		it("throws error if extend is called without a config object", function() {
+			expect(function() {
+				superschema.extend();
+			}).toThrowError("'config' has to be an object!");
+
+			expect(function() {
+				superschema.extend(137);
+			}).toThrowError("'config' has to be an object!");
+		});
+
+		it("throws error if no ko or knockout params given", function() {
+			expect(function() {
+				superschema.extend({});
+			}).toThrowError("superschema.extend called without any known parameters!");
+		});
+
+		it("throws error if extend is called with an invalid knockout object", function() {
+			expect(function() {
+				superschema.extend({
+					ko: 137
+				});
+			}).toThrowError("Invalid 'knockout' parameter given!");
+
+			expect(function() {
+				superschema.extend({
+					ko: {}
+				});
+			}).toThrowError("Invalid 'knockout' parameter given!");
+
+			expect(function() {
+				superschema.extend({
+					ko: {
+						isObservable: "not a function"
+					}
+				});
+			}).toThrowError("Invalid 'knockout' parameter given!");
+
+			expect(function() {
+				superschema.extend({
+					knockout: "not an object"
+				});
+			}).toThrowError("Invalid 'knockout' parameter given!");
+
+			expect(function() {
+				superschema.extend({
+					knockout: {}
+				});
+			}).toThrowError("Invalid 'knockout' parameter given!");
+
+			expect(function() {
+				superschema.extend({
+					knockout: {
+						isObservable: 42
+					}
+				});
+			}).toThrowError("Invalid 'knockout' parameter given!");
+
+		});
+	});
+
+	describe("invalid patterns", function() {
+		it ("throws error if the pattern isn't an object or a string", function() {
+			var pattern = 137;
 			expect(function() {
 				check(testObject, pattern);
-			}).toThrowError("The pattern to check has to be given as an object!");
+			}).toThrowError("Invalid pattern: 137");
 		});
 
 		it ("invalid pattern prop", function() {
@@ -64,34 +110,106 @@ describe("valid dependencies - with ko", function() {
 			};
 			expect(function() {
 				check(testObject, pattern);
-			}).toThrowError("The props of the pattern to check must be strings or objects!");
+			}).toThrowError("Invalid pattern: 666");
 		});
 
 		it ("invalid type in pattern", function() {
 			var pattern = {
 				a: {
-					type: true
+					__type: true
 				}
 			};
 			expect(function() {
 				check(testObject, pattern);
-			}).toThrowError("Invalid pattern: 'true' was given as type!");
+			}).toThrowError("Unknown type: true");
 		});
 
-		it ("throws error if the item to check is not an object", function() {
+		it ("unknown type in pattern", function() {
+			var pattern1 = {
+				a: {
+					__type: "fuction"
+				}
+			};
+			var pattern2 = {
+				a: "booolean"
+			};
 			expect(function() {
-				check("ugly, evil string", {});
-			}).toThrowError("The item to be checked has to be an object!");
+				check(testObject, pattern1);
+			}).toThrowError("Unknown type: fuction");
+			expect(function() {
+				check(testObject, pattern2);
+			}).toThrowError("Unknown type: booolean");
 		});
 
-		it ("uses the correct name in the error message if the item to check is not an object", function() {
+		it ("unknown type in pattern - deeper in the string", function() {
+			var pattern = {
+				j: "array numberd"
+			};
 			expect(function() {
-				check("ugly, evil string", {}, "My favourite parameter");
-			}).toThrowError("My favourite parameter has to be an object!");
+				check(testObject, pattern);
+			}).toThrowError("Unknown type: numberd");
+		});
+
+		it ("invalid pattern format", function() {
+			var pattern = {
+				b: "number number"
+			};
+			expect(function() {
+				check(testObject, pattern);
+			}).toThrowError("Invalid pattern: number number");
+		});
+
+		it ("throws error when trying observable checking without knockout", function() {
+			var pattern = {
+				a: {
+					__type: "observable"
+				}
+			};
+			expect(function() {
+				check(testObject, pattern);
+			}).toThrowError("Observable checking is not possible because no knockout instance is given!");
 		});
 	});
 
-	describe("simple required case", function() {
+	describe("simple string type definitions - without knockout", function() {
+		it("simple type checking - doesn't throw error if correct types are given", function() {
+			expect(function() {
+				check("beer", "string");
+				check(42, "number");
+				check(true, "boolean");
+				check([], "array");
+				check({}, "object");
+				check(function() {}, "function");
+			}).not.toThrow();
+		});
+
+		it("simple type checking - throws error on incorrect types", function() {
+			expect(function() {
+				check("beer", "array");
+			}).toThrowError(defaultName + " has to be an array!");
+			expect(function() {
+				check(42, "function");
+			}).toThrowError(defaultName + " should have function type!");
+			expect(function() {
+				check(true, "function");
+			}).toThrowError(defaultName + " should have function type!");
+			expect(function() {
+				check([], "function");
+			}).toThrowError(defaultName + " should have function type!");
+			expect(function() {
+				check({}, "function");
+			}).toThrowError(defaultName + " should have function type!");
+			expect(function() {
+				check(function() {}, "number");
+			}).toThrowError(defaultName + " should have number type!");
+		});
+
+		it ("simple type checking - uses the correct name if given", function() {
+			expect(function() {
+				check(42, "function", "My little function");
+			}).toThrowError("My little function should have function type!");
+		});
+
 		it ("doesn't throw error if the test object meets the pattern", function() {
 			var pattern = {
 				a: "string",
@@ -99,12 +217,12 @@ describe("valid dependencies - with ko", function() {
 				c: "object",
 				d: "array",
 				e: "function",
-				f: "observable number",
+				f: "function",
 				g: "object",
-				h: "observable object",
+				h: "function",
 				i: "boolean",
 				j: "array number",
-				k: "observable"
+				k: "function"
 			};
 			expect(function() {
 				check(testObject, pattern);
@@ -116,8 +234,8 @@ describe("valid dependencies - with ko", function() {
 				xxx: "boolean"
 			};
 			expect(function() {
-				check(testObject, pattern);
-			}).toThrowError("The 'xxx' property is mandatory!");
+				check(testObject, pattern, "myObject");
+			}).toThrowError("myObject.xxx is mandatory!");
 		});
 
 		it ("throws error if a property has incorrect type", function() {
@@ -125,11 +243,11 @@ describe("valid dependencies - with ko", function() {
 				a: "number"
 			};
 			expect(function() {
-				check(testObject, pattern);
-			}).toThrowError("The 'a' property should have number type!");
+				check(testObject, pattern, "myObject");
+			}).toThrowError("myObject.a should have number type!");
 		});
 
-		it ("throws error if a property should be an observable", function() {
+		/*it ("throws error if a property should be an observable", function() {
 			var pattern = {
 				b: "observable"
 			};
@@ -145,122 +263,137 @@ describe("valid dependencies - with ko", function() {
 			expect(function() {
 				check(testObject, pattern);
 			}).toThrowError("The 'f()' property should have string type!");
-		});
-
-		it ("throws error if an element should be an array", function() {
-			var pattern = {
-				a: "array"
-			};
-			expect(function() {
-				check(testObject, pattern);
-			}).toThrowError("The 'a' property has to be an array!");
-		});
+		});*/
 
 		it ("throws error if an element of an array has incorrect type", function() {
 			var pattern = {
 				d: "array number"
 			};
 			expect(function() {
-				check(testObject, pattern);
-			}).toThrowError("The 'd[1]' property should have number type!");
+				check(testObject, pattern, "myObject");
+			}).toThrowError("myObject.d[1] should have number type!");
 		});
 	});
 
-	describe("non-required properties", function() {
-		it ("doesn't throw error on missing non-required property", function() {
-			var pattern = {
-				xxx: {
-					required: false,
-					type: "Sith Lord"
-				}
-			};
-			expect(function() {
-				check(testObject, pattern);
-			}).not.toThrow();
+	describe("object format - without knockout", function() {
+		describe("__required and __nullable attributes", function() {
+			it("doesn't throw error on missing non-required property", function() {
+				var pattern = {
+					xxx: {
+						__required: false,
+						__type: "permutationOrbifold"
+					}
+				};
+				expect(function() {
+					check(testObject, pattern);
+				}).not.toThrow();
+			});
+
+			it("throws error on missing required property", function() {
+				var pattern = {
+					xxx: {
+						__required: true,
+						__type: "permutationOrbifold"
+					}
+				};
+				expect(function() {
+					check(testObject, pattern, "myObject");
+				}).toThrowError("myObject.xxx is mandatory!");
+			});
+
+			it("throws error if a non-required property has incorrect type", function() {
+				var pattern = {
+					a: {
+						__required: false,
+						__type: "number"
+					}
+				};
+				expect(function() {
+					check(testObject, pattern, "myObject");
+				}).toThrowError("myObject.a should have number type!");
+			});
+
+			it("properties are required by default", function() {
+				var pattern = {
+					xxx: {
+						__type: "object"
+					}
+				};
+				expect(function() {
+					check(testObject, pattern, "myObject");
+				}).toThrowError("myObject.xxx is mandatory!");
+			});
+
+			it("doesn't throw error if a nullable property has 'null' value", function() {
+				var pattern = {
+					l: {
+						__nullable: true,
+						__type: "object"
+					}
+				};
+				expect(function() {
+					check(testObject, pattern);
+				}).not.toThrow();
+			});
+
+			it("throws error if a non-nullable property has 'null' value", function() {
+				var pattern = {
+					l: {
+						__nullable: false,
+						__type: "object"
+					}
+				};
+				expect(function() {
+					check(testObject, pattern, "myObject");
+				}).toThrowError("myObject.l shouldn't be null!");
+			});
+
+			it("throws error if a nullable property has incorrect type", function() {
+				var pattern = {
+					a: {
+						__nullable: true,
+						__type: "object"
+					}
+				};
+				expect(function() {
+					check(testObject, pattern, "myObject");
+				}).toThrowError("myObject.a should have object type!");
+			});
+
+			it("properties are non-nullable by default", function() {
+				var pattern = {
+					l: {
+						__type: "object"
+					}
+				};
+				expect(function() {
+					check(testObject, pattern, "myObject");
+				}).toThrowError("myObject.l shouldn't be null!");
+			});
 		});
 
-		it ("throws error if a non-required property has incorrect type", function() {
+		it("doesn't throw error if types are correct", function() {
 			var pattern = {
 				a: {
-					required: false,
-					type: "number"
-				}
-			};
-			expect(function() {
-				check(testObject, pattern);
-			}).toThrowError("The 'a' property should have number type!");
-		});
-	});
-
-	describe("testing object types", function() {
-		it ("doesn't throw error on correct substructure", function() {
-			var pattern = {
-				g: {
-					type: {
-						g1: "string",
-						g2: "observable number",
-						g3: "number"
-					}
-				}
-			};
-			expect(function() {
-				check(testObject, pattern);
-			}).not.toThrow();
-		});
-
-		it ("throws error if the property to check is not an object", function() {
-			var pattern = {
-				a: {
-					type: {
-						xxx: "Sith Lord",
-					}
-				}
-			};
-			expect(function() {
-				check(testObject, pattern);
-			}).toThrowError("The 'a' property has to be an object!");
-		});
-
-		it ("throws error on missing required inner property", function() {
-			var pattern = {
-				g: {
-					type: {
-						xxx: "Sith Lord",
-					}
-				}
-			};
-			expect(function() {
-				check(testObject, pattern);
-			}).toThrowError("The 'g.xxx' property is mandatory!");
-		});
-	});
-
-	describe("long syntax", function() {
-		it ("doesn't throw error if types are correct", function() {
-			var pattern = {
-				a: {
-					required: true,
-					type: "string"
+					__required: true,
+					__type: "string"
 				},
 				b: {
-					observable: false,
-					required: true,
-					type: "number"
+					__required: true,
+					__type: "number"
+				},
+				c: {
+					__type: "object"
+				},
+				d: {
+					__type: "array"
+				},
+				e: {
+					__type: "function"
 				},
 				f: {
-					required: false,
-					observable: true,
-					type: "number"
-				},
-				h: {
-					observable: true,
-					type: {
-						h1: {
-							observable: true,
-							type: "number"
-						},
-						h2: "string"
-					}
+					__required: false,
+					__type: "function"
 				}
 			};
 			expect(function() {
@@ -268,17 +401,110 @@ describe("valid dependencies - with ko", function() {
 			}).not.toThrow();
 		});
 
-		it ("throws error if a type is incorrect", function() {
+		it("doesn't throw error on correct substructure", function() {
 			var pattern = {
-				a: {
-					required: true,
-					type: "number"
+				g: {
+					g1: "string",
+					g2: "function",
+					g3: "number"
 				}
 			};
 			expect(function() {
 				check(testObject, pattern);
-			}).toThrowError("The 'a' property should have number type!");
+			}).not.toThrow();
 		});
+
+		it("throws error if a type is incorrect", function() {
+			var pattern = {
+				a: {
+					__required: true,
+					__type: "number"
+				}
+			};
+			expect(function() {
+				check(testObject, pattern, "myObject");
+			}).toThrowError("myObject.a should have number type!");
+		});
+
+		it("checks array element types", function() {
+			var pattern = {
+				d: {
+					__type: "array",
+					__elements: "number"
+				}
+			};
+			expect(function() {
+				check(testObject, pattern, "myObject");
+			}).toThrowError("myObject.d[1] should have number type!");
+		});
+	});
+
+	describe("with knockout extension", function() {
+		beforeAll(function() {
+			superschema.extend({
+				ko: ko
+			});
+		});
+
+		it("doesn't throw error on observable check if knockout is given", function() {
+			var pattern = {
+				f: "observable",
+				h: "observable",
+				k: "observable"
+			};
+			expect(function() {
+				check(testObject, pattern);
+			}).not.toThrow();
+		});
+
+		it("checks observables correctly", function() {
+			var pattern1 = {
+				a: "observable"
+			};
+			var pattern2 = {
+				a: {
+					__type: "observable"
+				}
+			};
+			expect(function() {
+				check(testObject, pattern1, "myObject");
+			}).toThrowError("myObject.a has to be an observable!");
+			expect(function() {
+				check(testObject, pattern2, "myObject");
+			}).toThrowError("myObject.a has to be an observable!");
+		});
+
+		it("checks observable content", function() {
+			var pattern1 = {
+				f: "observable number"
+			};
+			var pattern2 = {
+				f: "observable string"
+			};
+			expect(function() {
+				check(testObject, pattern1);
+			}).not.toThrow();
+			expect(function() {
+				check(testObject, pattern2, "myObject");
+			}).toThrowError("myObject.f() should have string type!");
+		});
+
+		it("checks observable content - using object syntax", function() {
+			var pattern = {
+				h: {
+					__type: "observable",
+					__value: {
+						
+					}
+				}
+			};
+		});
+		
+	});
+
+	/*
+
+	describe("long syntax", function() {
 
 		it ("throws error if a property should be an observable", function() {
 			var pattern = {
@@ -341,13 +567,10 @@ describe("valid dependencies - with ko", function() {
 				check(testObject, pattern3);
 			}).not.toThrow();
 		});
-	});
+	});*/
 });
 
-describe("Valid dependencies - without ko", function() {
-	beforeEach(function() {
-		check = checkCreator();		
-	});
+/*describe("Valid dependencies - without ko", function() {
 
 	describe("test the parameters given", function() {
 		it ("throws error if the pattern isn't an object", function() {
@@ -610,4 +833,4 @@ describe("Valid dependencies - without ko", function() {
 			}).not.toThrow();
 		});
 	});
-});
+});*/
